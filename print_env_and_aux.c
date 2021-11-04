f332f9a8bbfbb7db10f1a8b3baa31d64c6c69be0
Chris Barts <chbarts@gmail.com>
2021-11-04 12:19:20 -0600

    Fix bug

--- print_env_and_aux.c
+++ print_env_and_aux.c
@@ -1,7 +1,33 @@
+#include <sys/auxv.h>
+#include <errno.h>
 #include <stdio.h>
+
+#define print_int_val(key, val) printf("%s: %ul\n", key, val)
+#define print_string_val(key, val) printf("%s: %s\n", key, (char *) val)
+
+static char *sarr[] =
+    { "AT_BASE", "AT_BASE_PLATFORM", "AT_CLKTCK", "AT_DCACHEBSIZE", "AT_EGID",
+    "AT_ENTRY", "AT_EUID", "AT_EXECFD", "AT_EXECFN", "AT_GID", "AT_HWCAP",
+    "AT_HWCAP2", "AT_ICACEBASESIZE", "AT_L1D_CACHEGEOMETRY", "AT_L1D_CACHESIZE",
+    "AT_L1I_CACHEGEOMETRY", "AT_L1I_CACHESIZE", "AT_L2_CACHEGEOMETRY",
+    "AT_L2_CACHESIZE", "AT_L3_CACHEGEOMETRY", "AT_L3_CACHESIZE", "AT_PAGESZ",
+    "AT_PHDR", "AT_PHENT", "AT_PHNUM", "AT_RANDOM", "AT_SECURE",
+    "AT_UCACHEBSIZE", "AT_UI", NULL
+};
+
+static unsigned long arr[] =
+    { AT_BASE, AT_BASE_PLATFORM, AT_CLKTCK, AT_DCACHEBSIZE, AT_EGID,
+    AT_ENTRY, AT_EUID, AT_EXECFD, AT_EXECFN, AT_GID, AT_HWCAP,
+    AT_HWCAP2, AT_ICACEBASESIZE, AT_L1D_CACHEGEOMETRY, AT_L1D_CACHESIZE,
+    AT_L1I_CACHEGEOMETRY, AT_L1I_CACHESIZE, AT_L2_CACHEGEOMETRY,
+    AT_L2_CACHESIZE, AT_L3_CACHEGEOMETRY, AT_L3_CACHESIZE, AT_PAGESZ,
+    AT_PHDR, AT_PHENT, AT_PHNUM, AT_RANDOM, AT_SECURE, AT_UCACHEBSIZE,
+    AT_UID, 0
+};
 
 int main(int argc, char *argv[])
 {
+    unsigned long val;
     int i;
 
     puts("Env:");
@@ -10,8 +36,22 @@ int main(int argc, char *argv[])
     }
 
     puts("Aux:");
-    for (i = i + 1; argv[i] != NULL; i++) {
-        puts(argv[i]);
+    for (i = 0; sarr[i] != NULL; i++) {
+        val = getauxval(arr[i]);
+        if ((0 == val) && (ENOENT == errno)) {
+            printf("%s not found.\n", sarr[i]);
+            continue;
+        }
+
+        switch (arr[i]) {
+        case AT_BASE_PLATFORM:
+        case AT_EXECFN:
+        case AT_PLATFORM:
+            print_string_val(sarr[i], val);
+            break;
+        default:
+            print_int_val(sarr[i], val);
+        }
     }
 
     return 0;
